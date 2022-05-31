c18fce648bec428a304b3ed3aacbdc2c3ada322b
Jeanna Vogt <jvogt@uw.edu>
2022-05-31 12:07:37 -0700

    ShibbolethAuthManager code cleanup and documentation.

--- src/Authentication/ShibbolethAuthManager.php
+++ src/Authentication/ShibbolethAuthManager.php
@@ -20,7 +20,9 @@ use Drupal\user\Entity\User;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\RequestStack;
 
-
+/**
+ * Handles the creation and destruction of Shibboleth sessions.
+ */
 class ShibbolethAuthManager {
 
   /**
@@ -86,6 +88,7 @@ class ShibbolethAuthManager {
    * @param \Drupal\Core\Routing\RouteMatchInterface          $current_route_match
    */
   public function __construct(ConfigFactoryInterface $config_factory, LoggerInterface $logger, RequestStack $request_stack, AccountInterface $current_user, MessengerInterface $messenger, RouteMatchInterface $current_route_match) {
+
     $this->config = $config_factory->get('shibboleth.settings');
     $this->logger = $logger;
     $this->requestStack = $request_stack;
@@ -93,20 +96,23 @@ class ShibbolethAuthManager {
 
   }
 
-  // public function authenticate() {
-  //
-  // }
-
-  // public function getSession() {
-  //   return $this->shib_session;
-  // }
-
+  /**
+   * Reports if there is or is not a Shibboleth session for the current user.
+   *
+   * @return bool
+   *   Returns TRUE if there is a Shibboleth session for the current user, FALSE
+   *   otherwise.
+   */
   public function sessionExists() {
     return !empty($this->getSessionId());
   }
 
   /**
+   * Gets the Shibboleth session ID.
+   *
    * @return string
+   *   Returns the Shibboleth session ID. If no session ID is found, it returns
+   *   NULL.
    */
   public function getSessionId() {
     if (!isset($this->sessionId)) {
@@ -116,7 +122,11 @@ class ShibbolethAuthManager {
   }
 
   /**
+   * Gets the username from the Shibboleth session.
+   *
    * @return string
+   *   Returns the username from the Shibboleth session. If no username is
+   *   found, it returns NULL.
    */
   public function getTargetedId() {
     if (!isset($this->targetedId)) {
@@ -126,7 +136,11 @@ class ShibbolethAuthManager {
   }
 
   /**
+   * Gets the email from the Shibboleth session.
+   *
    * @return string
+   *   Returns the email from the Shibboleth session. If no email is found, it
+   *   returns NULL.
    */
   public function getEmail() {
     if (!isset($this->email)) {
@@ -141,7 +155,11 @@ class ShibbolethAuthManager {
   }
 
   /**
+   * Gets the Identity Provider from the Shibboleth session.
+   *
    * @return string
+   *   Returns the Identity Provider from the Shibboleth session. If no Identity
+   *   Provider is found, it returns NULL.
    */
   public function getIdp() {
     if (!isset($this->idp)) {
@@ -151,8 +169,13 @@ class ShibbolethAuthManager {
   }
 
   /**
-   * @todo Pluralize!
+   * Gets a list of affiliations from the Shibboleth session.
+   *
    * @return array
+   *   Returns an array of affiliations. Returns an empty array if no
+   *   affiliations are found.
+   *
+   * @todo Pluralize!
    */
   public function getAffiliation() {
     if (!isset($this->affiliation)) {
@@ -161,14 +184,23 @@ class ShibbolethAuthManager {
     return $this->affiliation;
   }
 
+  /**
+   * Sets the affiliations from the Shibboleth session.
+   *
+   * @todo Pluralize!
+   */
   private function setAffiliation() {
     $affiliation = self::fixModRewriteIssues($this->config->get('server_variable_affiliation')) ?? self::fixModRewriteIssues('UNSCOPED_AFFILIATION');
     $this->affiliation = !empty($affiliation) ? explode(';', $affiliation) : [];
-    return $this->affiliation;
+    // return $this->affiliation;
   }
 
   /**
+   * Gets a list of groups that the user is a member of from the Shibboleth
+   * session.
+   *
    * @return array
+   *   Returns an array of groups. Returns an empty array if no groups are found.
    */
   public function getGroups() {
     if (!isset($this->groups)) {
@@ -177,6 +209,10 @@ class ShibbolethAuthManager {
     return $this->groups;
   }
 
+  /**
+   * Sets the list of groups that the user is a member of from the Shibboleth
+   * session.
+   */
   private function setGroups() {
     $groups = self::fixModRewriteIssues($this->config->get('server_variable_groups')) ?? self::fixModRewriteIssues('isMemberOf');
     $groups_arr = [];
@@ -189,12 +225,8 @@ class ShibbolethAuthManager {
       }
     }
     $this->groups = $groups_arr;
-    return $this->groups;
+    // return $this->groups;
   }
-
-  // public function createSession() {
-  //
-  // }
 
   // public function destroySession() {
   //
@@ -204,7 +236,7 @@ class ShibbolethAuthManager {
    * Gets the Shibboleth login handler URL.
    *
    * The URL can be relative or absolute. It won't include the login route or
-   * redirect.
+   * redirect. This configuration may be overridden in settings.php files.
    *
    * @see $this->getLoginUrl() for the full login URL.
    *
@@ -218,7 +250,7 @@ class ShibbolethAuthManager {
    * Gets the Shibboleth logout handler URL.
    *
    * The URL can be relative or absolute. It won't include the logout route or
-   * redirect.
+   * redirect. This configuration may be overridden in settings.php files.
    *
    * @see $this->getLogoutUrl() for the full logout URL.
    *
@@ -228,6 +260,14 @@ class ShibbolethAuthManager {
     return $this->config->get('shibboleth_logout_handler_url');
   }
 
+  /**
+   * Gets the full login URL including the handler, target and destination paths.
+   *
+   * Use of this path will attempt to log a Shibboleth user into Drupal.
+   *
+   * @return \Drupal\Core\Url
+   *   Returns the full login URL
+   */
   public function getLoginUrl() {
 
     // Set the destination to redirect to after successful login.
@@ -293,32 +333,29 @@ class ShibbolethAuthManager {
     return $login_url;
   }
 
+  /**
+   * Gets the URL for logging into Shibboleth and then redirecting to a path
+   * within the Drupal site.
+   *
+   * @return string
+   */
   public function getAuthenticateUrl() {
-    // Set the destination to redirect to after successful login.
-    $target = '';
-    // Use the configured redirect destination for all logins.
-    // if (!empty($this->config->get('login_redirect'))) {
-    //   $destination = Url::fromUserInput($this->config->get('login_redirect'))->toString();
-    // }
+
+    // First, check if the current route is the Shibboleth login page. This
+    // happens upon failure to log in to Drupal with a Shibboleth user.
+    // If so, set the destination to the original destination or the front page.
+    if ($this->currentRouteMatch->getRouteName() == 'shibboleth.drupal_login') {
+      return $this->getLoginUrl()->toString();
+    }
     // else {
-      // The login redirect isn't set in the configuration, so use the current
-      // path. The destination is local and absolute, always starting with a /.
-
-      // First, check if the current route is the Shibboleth login page. This
-      // happens upon failure to login to Drupal with a Shibboleth user.
-      // If so, set the destination to the original destination or the front page.
-      if ($this->currentRouteMatch->getRouteName() == 'shibboleth.drupal_login') {
-        // $destination = $this->requestStack->getCurrentRequest()->query->get('destination') ?? '<front>';
-        return $this->getLoginUrl();
-      }
-      else {
-        // Otherwise, use the current path as the destination.
-        // Grab the base path in case the site is a subsite.
-        $base_path = $this->requestStack->getCurrentRequest()->getBasePath();
-        $target = $base_path . $this->requestStack->getCurrentRequest()->getPathInfo();
-      }
-
+      // Set the target destination to redirect to after successful login.
+      // $target = '';
+      // Otherwise, use the current path as the destination.
+      // Grab the base path in case the site is a subsite.
+      $base_path = $this->requestStack->getCurrentRequest()->getBasePath();
+      $target = $base_path . $this->requestStack->getCurrentRequest()->getPathInfo();
     // }
+
     $target_options = [
       // Set this just in case, to make sure the destination starts with a /.
       'absolute' => TRUE,
@@ -326,24 +363,6 @@ class ShibbolethAuthManager {
         'target' => $target,
       ],
     ];
-
-    // Shibboleth will redirect to this 'target' route after successfully
-    // creating a new Shibboleth session.
-    // $auth_route = $authenticate_only ? 'shibboleth.authenticate' : 'shibboleth.drupal_login';
-    // $shib_login_url = Url::fromRoute('shibboleth.drupal_login', [], $target_options)->toString();
-    // $target_options = [
-    //   'query' => [
-    //     'target' => $shib_login_url,
-    //   ],
-    // ];
-
-    // $force_https = $this->config->get('force_https_on_login');
-    // if ($force_https) {
-    //   $target_options['https'] = TRUE;
-    //   if (empty($_SERVER['HTTPS'])) {
-    //     $target_options['absolute'] = TRUE;
-    //   }
-    // }
 
     $login_handler = $this->getLoginHandlerUrl();
     $login_url = '';
@@ -433,307 +452,7 @@ class ShibbolethAuthManager {
     // dpm($link, 'link');
     return $link;
   }
-//
-//   /**
-//    * @var
-//    */
-//   protected $user;
-//
-//   /**
-//    * @var \Drupal\Core\Entity\EntityStorageInterface
-//    */
-//   protected $user_store;
-//
-//
 
-//
-//   /**
-//    * @var \Drupal\Core\Session\SessionManagerInterface
-//    */
-//   protected $session_manager;
-//
-//   /**
-//    * @var \Drupal\Core\Session\AccountInterface
-//    */
-//   protected $current_user;
-//
-//   /**
-//    * @var string
-//    */
-//   protected $error_message;
-//
-//   /**
-//    * @var \Drupal\Core\Messenger\MessengerInterface
-//    */
-//   protected $messenger;
-//
-//   /**
-//    * MySQL error code for duplicate entry.
-//    */
-//   const MYSQL_ER_DUP_KEY = 23000;
-//
-//
-//   /**
-//    * LoginHandler constructor.
-//    *
-//    * @param \Drupal\Core\Database\Connection $db
-//    * @param \Drupal\Core\Config\ImmutableConfig $config
-//    * @param \Drupal\Core\Config\ImmutableConfig $adv_config
-//    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $etm
-//    * @param \Drupal\shib_auth\Login\ShibSessionVars $shib_session
-//    * @param \Psr\Log\LoggerInterface $logger
-//    * @param \Drupal\Core\TempStore\PrivateTempStoreFactory $temp_store_factory
-//    * @param \Drupal\Core\Session\SessionManagerInterface $session_manager
-//    * @param \Drupal\Core\Session\AccountInterface $current_user
-//    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
-//    *
-//    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-//    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
-//    */
-//   public function __construct(/*Connection $db, */ImmutableConfig $config, ImmutableConfig $adv_config, EntityTypeManagerInterface $etm, ShibSessionVars $shib_session, LoggerInterface $logger/*, PrivateTempStoreFactory $temp_store_factory*/, SessionManagerInterface $session_manager, AccountInterface $current_user, MessengerInterface $messenger) {
-//     // $this->db = $db;
-//     $this->config = $config;
-//     $this->adv_config = $adv_config;
-//     $this->user_store = $etm->getStorage('user');
-//     $this->shib_session = $shib_session;
-//     $this->logger = $logger;
-//     // $this->temp_store_factory = $temp_store_factory;
-//     $this->session_manager = $session_manager;
-//     $this->current_user = $current_user;
-//     $this->messenger = $messenger;
-//     // $this->custom_data_store = $this->temp_store_factory->get('shib_auth');
-//
-//     // Start Session if it does not exist yet.
-//     if ($this->current_user->isAnonymous() && !isset($_SESSION['session_started'])) {
-//       $_SESSION['session_started'] = TRUE;
-//       $this->session_manager->start();
-//     }
-//   }
-//
-//   /**
-//    * @return \Symfony\Component\HttpFoundation\RedirectResponse
-//    */
-//   public function shibLogin() {
-//
-//     try {
-//       $user_registered = FALSE;
-//       // Register new user if user does not exist.
-//       if (!$this->checkUserExists()) {
-//
-//         // Use the Shib email, if we've got it.
-//         // if (!empty($this->shib_session->getEmail())) {
-//         //   // Add custom Email to the session.
-//         //   $this->custom_data_store->set('custom_email', $this->shib_session->getEmail());
-//         // }
-//
-//         // Check if custom email has been set.
-//         // if (!$this->custom_data_store->get('custom_email')) {
-//         //   $this->custom_data_store->set('return_url', \Drupal::request()->getRequestUri());
-//         //   // Redirect to email form if custom email has not been set.
-//         //   $response = new RedirectResponse(Url::fromRoute('shib_auth.custom_data_form')
-//         //     ->toString());
-//         //   return $response;
-//         // }
-//         // else {
-//
-//         // }
-//         // return FALSE;
-//         // Check if there's a likely match with an existing Drupal user.
-//         // $user_match = user_load_by_name($this->shib_session->getTargetedId());
-//         if ($this->checkPotentialUserMatch()) {
-//           throw new \Exception('Shibboleth user is not mapped to a Drupal user, however, there is a potential matching account.', 11000);
-//           // Set the redirect to the Account map request form.
-//           // $redirect = Url::fromRoute('shib_auth.account_map_request');
-//           // echo $redirect;
-//           // return new RedirectResponse($redirect->toString());
-//         }
-//         else {
-//           $user_registered = $this->registerNewUser();
-//         }
-//       }
-//       else {
-//         $user_registered = TRUE;
-//       }
-//
-//       if ($user_registered) {
-//         $this->authenticateUser();
-//         return FALSE;
-//       }
-//
-//     }
-//     catch (\Exception $e) {
-//       // Log the error to Drupal log messages.
-//       $this->logger->error($e);
-//
-//       // Shibboleth user not mapped to a Drupal user, but a potential match
-//       // exists.
-//       if ($e->getCode() == 11000) {
-//         // $this->setErrorMessage(t('There was an error logging you in and we were unable to create a user for you.'));
-//         // $this->messenger->addError($this->getErrorMessage());
-//         return $e->getCode();
-//       }
-//       else {
-//         $user = \Drupal::currentUser();
-//         if ($user->isAuthenticated()) {
-//           // Kill the drupal session.
-//           // @todo - Do we need to kill the session for anonymous users, too? If so, how do we set the error message?
-//           user_logout();
-//         }
-//
-//         if ($this->getErrorMessage()) {
-//           $this->messenger->addError($this->getErrorMessage());
-//         }
-//
-//         $return_url = '';
-//         if ($this->adv_config->get('url_redirect_logout')) {
-//           $return_url = '?return=' . $this->adv_config->get('url_redirect_logout');
-//         }
-//         // Redirect to shib logout url.
-//         // @todo do we really want this?
-//         return new TrustedRedirectResponse($this->config->get('shibboleth_logout_handler_url') . $return_url);
-//       }
-//     }
-//     return FALSE;
-//   }
-//
-//   /**
-//    * Adds user to the shib_auth table in the database.
-//    *
-//    * @param bool $success
-//    *
-//    * @return bool
-//    *
-//    * @throws \Exception
-//    */
-//   private function registerNewUser($success = FALSE) {
-//     $user_data = [
-//       'name' => $this->shib_session->getTargetedId(),
-//       'mail' => $this->shib_session->getEmail(),
-//       'pass' => $this->genPassword(),
-//       'status' => 1,
-//       'shibboleth_username' => $this->shib_session->getTargetedId(),
-//     ];
-//
-//     try {
-//       // Create Drupal user.
-//       $this->user = $this->user_store->create($user_data);
-//       if (!$results = $this->user->save()) {
-//         // Throw exception if Drupal user creation fails.
-//         throw new \Exception();
-//       }
-//
-//     }
-//     catch (\Exception $e) {
-//       if ($e->getCode() == self::MYSQL_ER_DUP_KEY) {
-//         $this->setErrorMessage(t('There was an error creating your user. A user with your email address already exists.'));
-//         throw new \Exception('Error creating new Drupal user from Shibboleth Session. Duplicate user row.');
-//       }
-//       else {
-//         $this->setErrorMessage(t('There was an error creating your user.'));
-//         throw new \Exception('Error creating new Drupal user from Shibboleth Session.');
-//       }
-//     }
-//
-//     return TRUE;
-//   }
-//
-//   /**
-//    * Finalize user login.
-//    *
-//    * @return bool
-//    *
-//    * @throws \Exception
-//    */
-//   private function authenticateUser() {
-//     if (empty($this->user)) {
-//       $this->setErrorMessage(t('There was an error logging you in.'));
-//       throw new \Exception('No uid found for user when trying to initialize Drupal session.');
-//     }
-//     user_login_finalize($this->user);
-//     return TRUE;
-//   }
-//
-//   /**
-//    * Check shib_authmap table for user, return true if user found.
-//    *
-//    * @return bool
-//    *
-//    * @throws \Exception
-//    */
-//   public function checkUserExists() {
-//     $shib_user_lookup = $this->user_store
-//       ->loadByProperties([
-//         'shibboleth_username' => $this->shib_session->getTargetedId(),
-//       ]);
-//     $shib_user = reset($shib_user_lookup);
-//     // return $users ? reset($users) : FALSE;
-//     if (empty($shib_user)) {
-//
-//       // $this->setErrorMessage(t('There was an error logging you in.'));
-//       // throw new \Exception('No Drupal user found mapped to this Shibboleth username.');
-//       return FALSE;
-//     }
-//     $this->user = User::load($shib_user->id());
-//     // $user_query = $this->db->select('shib_authmap');
-//     // $user_query->fields('shib_authmap', ['id', 'uid', 'targeted_id']);
-//     // $user_query->condition('targeted_id', $this->shib_session->getTargetedId());
-//     // $results = $user_query->execute()->fetchAll();
-//     //
-//     // if (empty($results)) {
-//     //   // No user found.
-//     //   return FALSE;
-//     // }
-//     //
-//     // if (count($results) > 1) {
-//     //   $this->setErrorMessage(t('There was an error logging you in.'));
-//     //   throw new \Exception('Multiple entries for a user exist in the shib_authmap table.');
-//     // }
-//
-//     // $this->user = User::load($results[0]->uid);
-//     //
-//     // if (empty($this->user)) {
-//     //   $this->setErrorMessage(t('There was an error logging you in.'));
-//     //   throw new \Exception('User information exists in shib_authmap table, but Drupal user does not exist.');
-//     // }
-//     return TRUE;
-//   }
-//
-//   public function checkPotentialUserMatch() {
-//     $user_match = user_load_by_name($this->shib_session->getTargetedId());
-//     return $user_match ?? FALSE;
-//   }
-//
-//   /**
-//    * Generate a random password for the Drupal user account.
-//    *
-//    * @return string
-//    */
-//   private function genPassword() {
-//     $rand = new Random();
-//     return $rand->string(30);
-//   }
-//
-//   /**
-//    * @return \Drupal\shib_auth\Login\ShibSessionVars
-//    */
-//   public function getShibSession() {
-//     return $this->shib_session;
-//   }
-//
-//   /**
-//    *
-//    */
-//   private function setErrorMessage($message) {
-//     $this->error_message = $message;
-//   }
-//
-//   /**
-//    *
-//    */
-//   private function getErrorMessage() {
-//     return $this->error_message;
-//   }
-//
   /**
    * Get environment variables that may have been modified by mod_rewrite.
    *
