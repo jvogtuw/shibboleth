8297df174985b501539ccefcf45232c6dd0decfb
Jeanna Vogt <jvogt@uw.edu>
2022-06-01 14:05:03 -0700

    Re-ordered the excluded path options.

--- shibboleth_path/src/Form/ShibbolethPathSettings.php
+++ shibboleth_path/src/Form/ShibbolethPathSettings.php
@@ -35,10 +35,10 @@ class ShibbolethPathSettings extends ConfigFormBase {
       '#title' => $this->t('Exclude the following routes from Shibboleth protection'),
       '#description' => $this->t('These settings will override any path access rules that apply to them. They will not override Drupal permissions.<br>* Excluding the <strong>core user login</strong> route is recommended if enabling the "Whole site" path rule.'),
       '#options' => [
+        'shibboleth-drupal_logout' => t('Shibboleth logout from Drupal [/shibboleth/logout]'),
         'user-login' => t('Core user login [/user/login] (recommended*)'),
         'user-password' => t('Core user password reset [/user/password]'),
         'user-register' => t('Core user registration [/user/register]'),
-        'shibboleth-drupal_logout' => t('Shibboleth logout from Drupal [/shibboleth/logout]'),
       ],
       '#default_value' => $config->get('excluded_routes') ? $config->get('excluded_routes') : ['user-login'],
     ];
