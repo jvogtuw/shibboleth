e94c3df37964466daaf534d416dab7e6bfb96b7f
Jeanna Vogt <jvogt@uw.edu>
2022-06-07 16:34:43 -0700

    Coder compliance for LoginController.

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,7 +3,6 @@
 namespace Drupal\shibboleth\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Form\FormBuilderInterface;
 use Drupal\Core\Routing\TrustedRedirectResponse;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\shibboleth\Authentication\ShibbolethAuthManager;
@@ -16,13 +15,6 @@ use Symfony\Component\HttpFoundation\RequestStack;
  * credentials.
  */
 class LoginController extends ControllerBase {
-
-  /**
-   * Form builder service.
-   *
-   * @var \Drupal\Core\Form\FormBuilderInterface
-   */
-  protected $formBuilder;
 
   /**
    * @var \Psr\Log\LoggerInterface
@@ -47,17 +39,18 @@ class LoginController extends ControllerBase {
   /**
    * LoginController constructor.
    *
-   * @param ShibbolethAuthManager                          $shib_auth
-   * @param ShibbolethDrupalAuthManager                    $shib_drupal_auth
    * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
-   * @param \Drupal\Core\Form\FormBuilderInterface         $form_builder
+   *   The request stack.
+   * @param \Drupal\shibboleth\Authentication\ShibbolethAuthManager $shib_auth
+   *   The Shibboleth authentication manager.
+   * @param \Drupal\shibboleth\Authentication\ShibbolethDrupalAuthManager $shib_drupal_auth
+   *   The Shibboleth Drupal authentication manager.
    */
-  public function __construct(ShibbolethAuthManager $shib_auth, ShibbolethDrupalAuthManager $shib_drupal_auth, RequestStack $request_stack, FormBuilderInterface $form_builder) {
+  public function __construct(RequestStack $request_stack, ShibbolethAuthManager $shib_auth, ShibbolethDrupalAuthManager $shib_drupal_auth) {
 
+    $this->requestStack = $request_stack;
     $this->shibbolethAuthManager = $shib_auth;
     $this->shibbolethDrupalAuthManager = $shib_drupal_auth;
-    $this->requestStack = $request_stack;
-    $this->formBuilder = $form_builder;
     $this->logger = $this->getLogger('shibboleth');
   }
 
@@ -66,10 +59,9 @@ class LoginController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
+      $container->get('request_stack'),
       $container->get('shibboleth.auth_manager'),
       $container->get('shibboleth.drupal_auth_manager'),
-      $container->get('request_stack'),
-      $container->get('form_builder'),
     );
   }
 
@@ -80,6 +72,8 @@ class LoginController extends ControllerBase {
    * Drupal user that has been mapped to the Shibboleth authname.
    *
    * @return array|RedirectResponse
+   *   Returns a RedirectResponse upon successful login or a renderable array
+   *   upon failure.
    */
   public function login() {
 
@@ -87,13 +81,11 @@ class LoginController extends ControllerBase {
 
     // The user is logged into Shibboleth.
     if ($this->shibbolethAuthManager->sessionExists()) {
-
       $authname = $this->shibbolethAuthManager->getTargetedId();
       $id_label = $this->config('shibboleth.settings')->get('shibboleth_id_label');
 
       // The user is not logged into Drupal.
       if ($this->currentUser()->isAnonymous()) {
-
         // Attempt to log into Drupal with the Shibboleth ID.
         /** @var \Drupal\user\Entity\User|false $account */
         $account = $this->shibbolethDrupalAuthManager->loginRegister();
@@ -104,35 +96,30 @@ class LoginController extends ControllerBase {
         }
 
         // Login failed. Return access denied.
-        $this->messenger()->addError($this->t('Login failed via Shibboleth. We were unable to find or create a user linked to the @id_label <strong>%authname</strong>. Please contact the site administrator to request access.', ['@id_label' => $id_label, '%authname' => $authname]));
+        $this->messenger()->addError($this->t('Login failed via Shibboleth. We were unable to find or create a user linked to the @id_label <strong>%authname</strong>. Please contact the site administrator to request access.',
+          ['@id_label' => $id_label, '%authname' => $authname]));
         return $this->loginError();
-
       }
       else {
-
         $current_user_authname = $this->shibbolethDrupalAuthManager->getShibbolethAuthname($this->currentUser()->id());
 
         // Check if Shibboleth user matches Drupal user.
         if ($current_user_authname == $authname || $this->currentUser()->hasPermission('bypass shibboleth login')) {
-
           $this->messenger()->addStatus($this->t('You are already logged in.'));
           return new RedirectResponse($destination);
-
         }
         elseif (!$this->currentUser()->hasPermission('bypass shibboleth login')) {
-
           // The Shibboleth and Drupal user authnames don't match and the Drupal
           // user doesn't have permission to bypass Shibboleth login.
-          $this->messenger()->addError($this->t('You have been logged out of this site because the @id_label <strong>%authname</strong> did not match the Drupal user and the Drupal user did not have permission to bypass Shibboleth login. You can try to log in again. Please contact the site administrator for more information.', ['@id_label' => $id_label, '%authname' => $authname]));
+          $this->messenger()->addError($this->t('You have been logged out of this site because the @id_label <strong>%authname</strong> did not match the Drupal user and the Drupal user did not have permission to bypass Shibboleth login. You can try to log in again. Please contact the site administrator for more information.',
+            ['@id_label' => $id_label, '%authname' => $authname]));
           return $this->loginError();
-
         }
       }
     }
     $this->logger->notice('login redirecting to external.');
     // No Shibboleth session exists, so redirect to the Shibboleth login URL.
     return new TrustedRedirectResponse($this->shibbolethAuthManager->getLoginUrl()->toString());
-
   }
 
   /**
@@ -142,12 +129,10 @@ class LoginController extends ControllerBase {
    *   Returns a render array.
    */
   protected function loginError() {
-
     return [
       '#title' => t('Access denied'),
-      '#markup' => t('Login failed.')
+      '#markup' => t('Login failed.'),
     ];
-
   }
 
 }
