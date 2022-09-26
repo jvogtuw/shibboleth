264f4080950c695be26a7d8a29bcc4ea3359335f
Jeanna Vogt <jvogt@uw.edu>
2022-09-26 10:45:12 -0700

    Removed dependency on symfony/config.

--- src/Exception/ShibbolethSessionException.php
+++ src/Exception/ShibbolethSessionException.php
@@ -2,11 +2,9 @@
 
 namespace Drupal\shibboleth\Exception;
 
-use Symfony\Component\Config\Definition\Exception\Exception;
-
 /**
  * Exception thrown when no Shibboleth session exists.
  */
-class ShibbolethSessionException extends Exception {
+class ShibbolethSessionException extends \Exception {
 
 }
